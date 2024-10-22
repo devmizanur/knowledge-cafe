a45098bc7aa0a129729aa330b6fc9ded3c1a590d
devmizanur <sohag.bogra420@gmail.com>
2024-10-22 13:01:50 +0600

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-
-      <h1 className='text-3xl font-bold text-green-400'>Knowledge Cafe</h1>
+  <Header></Header>
+  <div className='flex'>
+  <Blogs></Blogs>
+  <Bookmarks></Bookmarks>
+    </div>
 
     </>
   )
